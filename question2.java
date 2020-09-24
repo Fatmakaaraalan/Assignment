97f16ab512c34d6bc1adf4b2c89fa8111ebe7d99
Fatmakaaraalan <71496187+Fatmakaaraalan@users.noreply.github.com>
2020-09-24 20:03:47 +0100

    Add files via upload

---
+++ question2.java
@@ -0,0 +1,21 @@
+package assignment3;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+	   // number + string 
+	
+	   int  basedOnYourInPut, YourAgeIs;
+	   	int num1,num2,sum;
+		num2=1979;
+		num1=2020;
+		sum=num1-num2;
+	
+		System.out.print("hello Fatma ! Based On Your In Put,Your Age Is " );
+		System.out.println(sum = num1-num2);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- question7.java
+++ question7.java
@@ -3,10 +3,19 @@ package assignment3;
 public class question7 {
 
 	public static void main(String[] args) {
+		int paid=1;
+		int cent=100*paid;
+		int dimes=10*cent;
+		int quarters=paid*25;
+		int nickles;
+		System.out.println(dimes);
+		System.out.println("hi you have paid me " + paid*3 + " dolars ");
+		System.out.println("you have purchased 3 items worth " + 2*paid + " dolars " + 3*quarters + " cents ");
+		System.out.println("therefore i need to refund back " + 1*paid + " quarters " + 0*20 + " dimes " + 0*7 + " nickles");
 		
-		
-		// TODO Auto-generated method stub
 
+		
+		
 	}
 
 }
